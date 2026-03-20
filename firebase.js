5aa5f538f10ff19eebd63b3eed6321e04ca271c2
Prince Verma <142138695+princeverma10@users.noreply.github.com>
2026-03-20 19:26:31 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// PASTE YOUR FIREBASE CONFIG HERE
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
