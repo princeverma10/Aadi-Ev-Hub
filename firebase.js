647b58fff5dd7e344d2d401996f12b778c65f9b3
Prince Verma <142138695+princeverma10@users.noreply.github.com>
2026-03-20 19:37:59 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-// PASTE YOUR FIREBASE CONFIG HERE
-const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
